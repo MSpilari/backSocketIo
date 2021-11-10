1a15636c25a61b664cb86ed7ffa386f8d3735c64
MSpilari <mb.spilari@hotmail.com>
2021-11-10 12:48:07 -0300

    Criando a rota de envio de mensagens

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
+import { CreateMessageController } from './controllers/CreateMessageController'
+import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
 const router = Router()
 
@@ -16,5 +18,11 @@ router.get('/signin/callback', (req, res) => {
 })
 
 router.post('/authenticate', new AuthenticateUserController().handle)
+
+router.post(
+	'/messages',
+	ensureAuthenticated,
+	new CreateMessageController().handle
+)
 
 export { router }
