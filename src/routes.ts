e30cc2865c66c592c79c410dd0a8ab3f3a0fdfd7
MSpilari <mb.spilari@hotmail.com>
2021-11-10 11:07:50 -0300

    Criando a rota signin e a authenticate

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,20 @@
-import express from 'express'
+import { Router } from 'express'
+import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 
-const router = express.Router()
+const router = Router()
 
 router.get('/github', (req, res) => {
 	res.redirect(
 		`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`
 	)
 })
+
+router.get('/signin/callback', (req, res) => {
+	const { code } = req.query
+
+	return res.json(code)
+})
+
+router.post('/authenticate', new AuthenticateUserController().handle)
 
 export { router }
