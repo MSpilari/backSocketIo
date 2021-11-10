9806dc7514c2162d6abddb2c85851e26c37eb7f3
MSpilari <mb.spilari@hotmail.com>
2021-11-10 11:07:06 -0300

    Inserindo as variáveis de ambiente e o parser para json

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config'
 import express from 'express'
 import { router } from './routes'
 
 const app = express()
 
+app.use(express.json())
 app.use(router)
 
 app.listen(4000, () => console.log('Server is running on PORT 4000'))
