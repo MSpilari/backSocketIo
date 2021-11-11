57f1ffe321a02d2915b5703b693fcb89b0019898
MSpilari <mb.spilari@hotmail.com>
2021-11-11 11:34:09 -0300

    Criando as rotas que mostram as 3 últimas msgs e mostra o usuário

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { AuthenticateUserController } from './controllers/AuthenticateUserController'
 import { CreateMessageController } from './controllers/CreateMessageController'
+import { GetLast3MessagesController } from './controllers/GetLast3MessagesController'
+import { ProfileUserController } from './controllers/ProfileUserController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
 const router = Router()
@@ -24,5 +26,9 @@ router.post(
 	ensureAuthenticated,
 	new CreateMessageController().handle
 )
+
+router.get('/messages/last3', new GetLast3MessagesController().handle)
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
